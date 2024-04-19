0b2cc6d4972663ea6d0c4dd11c2fdee62fddbde1
jelly3105 <anjalisherikar3105@gmail.com>
2024-04-19 13:28:39 +0530

    sign up functionality

---
+++ src/models/user.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// Schama means what fields will be there in the user collection and their types.
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    }
+});
+
+// Convert Schema into Model
+const User = mongoose.model('User',userSchema);
+
+export default User;
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,12 +1,36 @@
 import express, { Router, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../models/user";
 const authRouter:Router = express.Router();
+dotenv.config();
+
+const secretKey = process.env.JWTSECRET as string;
+console.log(secretKey);
+
 
 authRouter.post('/api/signup', async (req: Request, res: Response) => {
     try{
         const {name, email, role} = req.body;
-        res.send("Sign up API")
-    }catch(e){
-        res.status(500).send(e);
+        // Check if Email already exists
+        let user = await User.findOne({email: email});
+
+        if(user) {
+            res.send("User already exists! Please login!")
+        }
+
+        // If user doesnot exist
+        user = new User({
+            name: name,
+            email: email,
+            role: role
+        });
+        user = await user.save();  
+        
+        const token = jwt.sign({id: user._id},secretKey);
+        res.json({user: user,token:token}); // status 200 is sent by default
+    }catch(e:any){
+        res.status(500).json({error: e.message});
     }
    
 })
